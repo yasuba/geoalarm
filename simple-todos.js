4441a5a2c3a8aeb5efde41660f333fa3521d1c87
Maya Driver <madriver@deloitte.co.uk>
2015-03-10 21:40:15 +0000

    list items can be added through form and auto update the db and the frontend - wow!

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,25 @@
-if (Meteor.isClient) {
+Tasks = new Mongo.Collection("tasks");
+
+ if (Meteor.isClient) {
 
   Template.body.helpers({
-    tasks: [
-    {text: "This is task 1"},
-    {text: "This is task 2"},
-    {text: "This is task 3"}
-    ]
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text:text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      return false;
+    }
   });
 }
